e7b35a2e277c7b7545502572d7764710503d48e1
Sermadurai1997 <124354534+Sermadurai1997@users.noreply.github.com>
2023-05-06 15:44:31 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+//Create Typing animation
+
+var typed = new Typed(".typing",{
+    strings : ["Fresher","Software developer","MERN Stack Developer","Python Developer","Web designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+
+})
+
+var typed = new Typed(".typing-2",{
+    strings : ["Software developer","Fresher","Python Developer","Web designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+
+})
